c2844bd02c54d57a11ba932b6c48f853cdd54a28
Holger Kruse <mail2holger@gmail.com>
2018-11-27 21:24:36 +0100

    .

--- NAF-MP2.py
+++ NAF-MP2.py
@@ -3,17 +3,27 @@ A reference implementation of density-fitted MP2 from a RHF reference.
 with added simple NAF approximiation
 http://aip.scitation.org/doi/10.1063/1.4905005
 
+procedure:
+J=Qov=(Q|ia)
+
+paper equations: (mind that PSI4's "J" is (Q|ia), paper has (ia|Q))
+1. form W=Jt*J
+2. diag W
+3. select significant eigenvectors of W in matrix N_bar
+4. form new J J_bar=J*N_bar
+5. calculated MP2 energy with new J=J_bar
+
 References: 
 Algorithm modified from Rob Parrish's most excellent Psi4 plugin example
 Bottom of the page: http://www.psicode.org/developers.php
 """
 
-__authors__   = "Daniel G. A. Smith"
-__credits__   = ["Daniel G. A. Smith", "Dominic A. Sirianni"]
+__authors__ = "Daniel G. A. Smith"
+__credits__ = ["Daniel G. A. Smith", "Dominic A. Sirianni"]
 
 __copyright__ = "(c) 2014-2018, The Psi4NumPy Developers"
-__license__   = "BSD-3-Clause"
-__date__      = "2017-05-23"
+__license__ = "BSD-3-Clause"
+__date__ = "2017-05-23"
 
 import time
 import numpy as np
@@ -44,8 +54,7 @@ symmetry c1
 #psi4.set_options({'basis': 'cc-pVDZ',
 #                  'df_basis_scf': 'cc-pvdz-ri'})
 
-psi4.set_options({'basis': 'minix',
-                  'df_basis_scf': 'def2-SVP-RI'})
+psi4.set_options({'basis': 'minix', 'df_basis_scf': 'def2-SVP-RI'})
 check_energy = False
 #check_energy = True
 
@@ -68,82 +77,78 @@ print('\nBuilding DF ERI tensor Qov...')
 t = time.time()
 C = wfn.Ca()
 aux = psi4.core.BasisSet.build(mol, "DF_BASIS_MP2", "", "RIFIT", "aug-cc-pvdz")
-df = psi4.core.DFTensor(wfn.basisset(), aux, C, ndocc, nvirt) 
+df = psi4.core.DFTensor(wfn.basisset(), aux, C, ndocc, nvirt)
 # Transformed MO DF tensor
 Qov = np.asarray(df.Qov())
 print('...Qov build in %.3f seconds with a shape of %s, %.3f GB.' \
 % (time.time() - t, str(Qov.shape), np.prod(Qov.shape) * 8.e-9))
 
-# NAF start
-#  calculate W Matrix = W=Jt*J 
+########## NAF start ###############
+#  calculate W Matrix = W=Jt*J
 print(' ** NAF  **')
-naux=aux.nbf()
-print('computing W matrix with dimension naux = %i' %(naux))
+naux = aux.nbf()
+print('computing W matrix with dimension naux = %i' % (naux))
 # reshape to form J (Q|ia)
-J =np.reshape(Qov,(naux,nvirt*ndocc))
+J = np.reshape(Qov, (naux, nvirt * ndocc))
 
 # eq. 5 but transposed (Q|Q)=(Q|ia)(ia|Q)
-W=np.dot(J,J.T) 
+W = np.dot(J, J.T)
 
 print('diagonalization of W ')
-#e_val,e_vec=np.linalg.eig(W)
-e_val,e_vec=np.linalg.eigh(W)
+e_val, e_vec = np.linalg.eigh(W)
 #  construct modified Qov matrix Nbar=(Qbar|Q) from selected eigenvectors of W
 print('selecting eigenvectors ')
-ethr=1e-12
-nskipped=0
-Nbar=np.zeros((naux,naux))
-niter=0
-naux2=0
+# ethr threshold is supposed to be 10^-4 ???
+ethr = 1e-18   
+nskipped = 0
+Ntmp = np.zeros((naux, naux))
+niter = 0
+naux2 = 0
 
 # sanity check if transformations are OK
-for n in range(naux):
-   Nbar[naux2,:]=e_vec[n,:]
-   naux2+=1
+#for n in range(naux):
+#    Nbar[naux2, :] = e_vec[n, :]
+#    naux2 += 1
 
-naux2=0
+tmp = []
+naux2 = 0
 for n in range(naux):
-    if(e_val[n]>=ethr):
-        Nbar[naux2,:]=e_vec[n,:]
-        naux2+=1
+#    print(e_val[n])
+    if (abs(e_val[n]) >= ethr):
+#    if (e_val[n] >= 0.0):
+        Ntmp[naux2, :] = e_vec[n, :]
+        naux2 += 1
 
-#naux2=naux-50
-# resizing Nbar. 
 print('new naux = %i  ' % (naux2))
-Nbar=np.resize(Nbar,(naux2,naux))
+Nbar = Ntmp[0:naux2,0:naux] 
 
 # eq.6 Qov_bar=Jbar=(Qbar|ia)=(Qbar|Q)*(Q|ia)
 print('computing new Qov ')
-Qov_bar=np.dot(Nbar,J)
-print('shape Jbar :',Qov_bar.shape)
-Qov=np.zeros((naux2,ndocc,nvirt))
+Qov_bar = np.dot(Nbar, J)
+print('shape Jbar :', Qov_bar.shape)
+Qov = np.zeros((naux2, ndocc, nvirt))
 # rebuild 3-index Qov
 # numpy solution?
-# Qov=np.reshape(Qov_bar,(naux2,ndocc,nvirt))  # can i do this?
-for q in range(naux2):
-    for i in range(ndocc):
-        for a in range(nvirt):
-            qia=(q,i,a)
-            idx=np.unravel_index(np.ravel_multi_index(qia,Qov.shape), Qov_bar.shape ) 
-#            print(qia,idx)
-            Qov[q,i,a]=Qov_bar[idx]
+Qov=np.reshape(Qov_bar,(naux2,ndocc,nvirt))  # can i do this?
+#for q in range(naux2):
+#    for i in range(ndocc):
+#        for a in range(nvirt):
+#            qia = (q, i, a)
+#            idx = np.unravel_index(
+#                np.ravel_multi_index(qia, Qov.shape), Qov_bar.shape)
+#            #            print(qia,idx)
+#            Qov[q, i, a] = Qov_bar[idx]
 
 # print(Qov)
-print('shape Qov:',Qov.shape)
+print('shape Qov:', Qov.shape)
+########## NAF end  ###############
 
 print('\nComputing MP2 energy...')
 t = time.time()
-
-# At this point we can trivially build the ovov MO tensor and compute MP2
-# identically to that as MP2.dat. However, this means we have to build the
-# 4-index ERI tensor in memory and results in little gains over conventional
-# MP2
-# MO = np.einsum('Qia,Qjb->iajb', Qov, Qov)
-
 # A smarter algorithm, loop over occupied indices and exploit ERI symmetry
 
 # This part of the denominator is identical for all i,j pairs
-vv_denom = - eps_vir.reshape(-1, 1) - eps_vir
+vv_denom = -eps_vir.reshape(-1, 1) - eps_vir
 
 MP2corr_OS = 0.0
 MP2corr_SS = 0.0
@@ -156,7 +161,7 @@ for i in range(ndocc):
         j_Qv = Qov[:, j, :]
 
         # We can either use einsum here
-#        tmp = np.einsum('Qa,Qb->ab', i_Qv, j_Qv)
+        #        tmp = np.einsum('Qa,Qb->ab', i_Qv, j_Qv)
 
         # Or a dot product (DGEMM) for speed)
         tmp = np.dot(i_Qv.T, j_Qv)
@@ -179,10 +184,6 @@ print('...finished computing MP2 energy in %.3f seconds.' % (time.time() - t))
 MP2corr_E = MP2corr_SS + MP2corr_OS
 MP2_E = RHF_E + MP2corr_E
 
-# These are the canonical SCS MP2 coefficients, many others are available however
-SCS_MP2corr_E = MP2corr_SS / 3 + MP2corr_OS * (6. / 5)
-SCS_MP2_E = RHF_E + SCS_MP2corr_E
-
 #print('\nMP2 SS correlation energy:         %16.10f' % MP2corr_SS)
 #print('MP2 OS correlation energy:         %16.10f' % MP2corr_OS)
 
@@ -190,13 +191,5 @@ print('\nMP2 correlation energy:            %16.10f' % MP2corr_E)
 print('MP2 total energy:                  %16.10f' % MP2_E)
 
 psi4.energy('MP2')
-ecorr=psi4.core.get_variable('MP2 CORRELATION ENERGY')
-print('reference Ecorr(MP2) = %f ; error = %f ' %(ecorr,ecorr-MP2corr_E))
-#print('\nSCS-MP2 correlation energy:        %16.10f' % MP2corr_SS)
-#print('SCS-MP2 total energy:              %16.10f' % SCS_MP2_E)
-
-if check_energy:
-    psi4.energy('MP2')
-    psi4.compare_values(psi4.core.get_variable('MP2 TOTAL ENERGY'), MP2_E, 5, 'MP2 Energy')
-    psi4.compare_values(psi4.core.get_variable('SCS-MP2 TOTAL ENERGY'), SCS_MP2_E, 5, 'SCS-MP2 Energy')
-
+ecorr = psi4.core.get_variable('MP2 CORRELATION ENERGY')
+print('reference Ecorr(MP2) = %f ; error = %f ' % (ecorr, ecorr - MP2corr_E))
